b2a19f1f123fd48c84e48eb6ee6110d599e494db
denningkevin <denningkev@gmail.com>
2019-10-28 21:35:21 +0300

    create quote-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteDetailComponent
   ],
   imports: [
     BrowserModule,
