a142de86ea64608b9cec9eba6a5aea5c92233558
denningkevin <denningkev@gmail.com>
2019-10-28 21:47:00 +0300

    add quotes to quote.components.ts

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
+  quotes: Quote[] = [
+    new Quote(1, 'Simplicity is the ultimate sophistication', 'Frankline Baraka', new Date(2019, 1, 19)),
+    new Quote(2, 'If you judge people, you have no time to love them', 'Mother Theresa', new Date(2019, 1, 19)),
+    new Quote(3, 'A great man is always willing to be little', 'Ralph Waldo Emerson', new Date(2019, 1, 19)),
+    new Quote(4, 'The greatest wealth is to live content with little', 'Plato', new Date(2019, 8, 18)),
+  ];
   constructor() { }
 
   ngOnInit() {
